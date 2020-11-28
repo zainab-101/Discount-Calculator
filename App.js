b3e42fe5358f044ad69276248893f4f1e65d1725
zainab-101 <zainab.gulab97@gmail.com>
2020-11-28 23:16:56 +0500

    Updated after task #4

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { setStatusBarStyle, StatusBar } from 'expo-status-bar';
 import React , {useState} from 'react';
-import { StyleSheet, Text, TextInput, View, TouchableOpacity} from 'react-native';
+import logo from './assets/logo.jpg'; 
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, Image} from 'react-native';
 
 
 export default function App() {
@@ -10,45 +11,56 @@ export default function App() {
 
   let SavedAmount;
   let FinalPrice;
+  let Error="";
   const Calculate = () => {
     const OriginalP = parseFloat(OriginalPrice);
-    const DiscountP = parseFloat(DiscountPercentage)/100;
-    FinalPrice = OriginalP - (OriginalP * DiscountP).toFixed(2);
-    SavedAmount = OriginalP - FinalPrice;
+    const DiscountP = parseFloat(DiscountPercentage);
+    if(DiscountP<100){
+      FinalPrice = parseFloat(OriginalP - (OriginalP * DiscountP/100).toFixed(2));
+      SavedAmount = parseFloat(OriginalP - FinalPrice);
+    }
+    else{
+      Error = "Discount greater than 100";
+    }
   }
 
   return (
     <View style={styles.container}>
+      <Image
+      style={{width: "50%", height: "40%"}} source={logo}
+      />
       <View>
-        <Text style={{ fontSize: 20, fontWeight: 'bold', color:"#2C302E"}}>Discount Calculator</Text>
+        <Text style={{ fontSize: 40, fontFamily: "lucida grande", fontWeight: 'bold', color:"#DFF1E8"}}>Discount Calculator</Text>
       </View>
       <View style={{width: "40%"}}>
-        <Text style={{ fontSize: 20, fontWeight: 'bold', color:"#2C302E" }}>Original Price:</Text>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', color:"#DFF1E8" }}>Original Price:</Text>
         <TextInput
-        style={{ height: 40, borderColor: '#2C302E', borderWidth: 3, borderRadius: 20 }}
+        style={{ height: 40, color:"#DFF1E8", borderColor: '#2C302E', borderWidth: 3, borderRadius: 20, textAlign: "center", fontWeight: 'bold' }}
         value={OriginalPrice} 
+        keyboardType='numeric'
         onChangeText={text => setOriginalPrice(text)}/>
       </View>
       <View style={{width: "40%"}}>
-        <Text style={{ fontSize: 20, fontWeight: 'bold', color:"#2C302E" }}>Discount %:</Text>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', color:"#DFF1E8" }}>Discount %:</Text>
         <TextInput
-        style={{ height: 40, borderColor: '#2C302E', borderWidth: 3, borderRadius: 20  }}
+        style={{ height: 40, color:"#DFF1E8", borderColor: '#2C302E', borderWidth: 3, borderRadius: 20, textAlign: "center", fontWeight: 'bold'  }}
         value={DiscountPercentage}
+        keyboardType='numeric'
         onChangeText={text => setDiscountPercentage(text)} />
       </View>
-      <TouchableOpacity 
-        style={{justifyContent: "space-between", width: "40%", marginTop:"5%"}}
-        onPress={Calculate()}>
-        <Text style={{textAlign:"center", fontWeight: 'bold', fontSize:25, color:"#404644", backgroundColor:"#24BA88", borderRadius: 20}}>Calculate</Text>
-      </TouchableOpacity>
+      <Text value={Calculate()} style={{ fontSize: 30, fontFamily: "lucida grande", fontWeight: 'bold', color:"#DFF1E8"}}>After Discount:</Text>
       
-      <View style={{width: "40%"}}>
-        <Text style={{ fontSize: 20, fontWeight: 'bold', color:"#2C302E" }}>You Saved:</Text>
-        {SavedAmount}
+      <View style={{width: "40%", color:"#DFF1E8"}}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', color:"#DFF1E8" }}>You Saved: {SavedAmount}</Text>
+        
       </View>
-      <View style={{width: "40%"}}>
-        <Text style={{ fontSize: 20, fontWeight: 'bold', color:"#2C302E" }}>Final Price:</Text>
-        {FinalPrice}
+      <View style={{width: "40%", color:"#DFF1E8"}}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', color:"#DFF1E8" }}>Final Price: {FinalPrice}</Text>
+        
+      </View>
+      <View style={{width: "40%", color:"#D80707"}}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', color:"#DFF1E8" }}>Error Message: {Error}</Text>
+        
       </View>
       <StatusBar style="auto"/>
     </View>
